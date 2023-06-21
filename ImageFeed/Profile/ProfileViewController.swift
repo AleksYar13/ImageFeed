756c52a81e01c3caae8add1c4cd609a39093b5ba
AleksYar13 <yareshko69@gmail.com>
2023-06-21 15:25:06 +0300

    add constraints for Profile

---
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Александр Ярешко on 21.06.2023.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    @IBOutlet private var avatarImageView: UIImageView!
+    @IBOutlet private var nameLabel: UILabel!
+    @IBOutlet private var loginNameLabel: UILabel!
+    @IBOutlet private var descriptionLabel: UILabel!
+
+    @IBOutlet private var logoutButton: UIButton!
+
+    @IBAction private func didTapLogoutButton() {
+    }
+}
+
